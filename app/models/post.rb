df6bcda02005c29366b44d905a4217ed9cdd5d12
LandonWest <landonwest5@gmail.com>
2014-11-23 15:40:04 -0800

    add to_param and slug methods to make vanity urls for seo

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
 
   validates :title, :author, :body, presence: true
 
+  def slug
+    title.downcase.gsub(" ", "-")
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
 end
